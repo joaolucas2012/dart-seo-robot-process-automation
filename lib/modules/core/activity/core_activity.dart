f7a8e0ce8d799819cac372b68d6d827a01211e8f
@joaolucas-prime <jlucas@grupo-artico.com>
2023-04-27 20:02:21 -0300

    :fire: [FIX] fix CoreActivity

--- lib/modules/core/activity/core_activity.dart
+++ lib/modules/core/activity/core_activity.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:dart_seo_robot/modules/keyword_tool_page/activity/keyword_tool_activity.dart';
 import 'package:dart_seo_robot/modules/rapid_tags_page/activity/rapid_tags_activity.dart';
 import 'package:dart_seo_robot/modules/shared/bot/bot.dart';
-import 'package:dart_seo_robot/modules/shared/classes/navigator.dart';
 
 class CoreActivity {
   Future<void> _runCoreActivity() async {
