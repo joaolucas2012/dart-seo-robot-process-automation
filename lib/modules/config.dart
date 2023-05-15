7d88b1506585f1e6592a0b05b027dc75f73cf849
@joaolucas-prime <jlucas@grupo-artico.com>
2023-05-15 17:28:06 -0300

    :rocket: [FEAT] implemented an activity to get user entries to use in the process of keywords searching

--- lib/modules/config.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dart_seo_robot/modules/env.dart';
-
-abstract class Config {
-  static String get searchKey => SEARCH_KEY;
-
-  static bool get isHeadless => IS_HEADLESS;
-}
--- lib/modules/core/activity/core_activity.dart
+++ lib/modules/core/activity/core_activity.dart
@@ -1,3 +1,4 @@
+import 'package:dart_seo_robot/modules/get_entries/activity/get_entries_activity.dart';
 import 'package:dart_seo_robot/modules/keyword_tool_page/activity/keyword_tool_activity.dart';
 import 'package:dart_seo_robot/modules/rapid_tags_page/activity/rapid_tags_activity.dart';
 import 'package:dart_seo_robot/modules/shared/bot/bot.dart';
@@ -10,6 +11,7 @@ class CoreActivity {
 
   Future<void> _runCoreActivity() async {
     try {
+      await GetEntriesActivity().start();
       await KeywordToolActivity().start();
       await RapidTagsActivity().start();
       await Bot.destroy();
